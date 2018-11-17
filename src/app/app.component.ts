9c82820ab186153050e130660288231c26ea8340
Katarzyna Romaszewska <kotryn1415@gmail.com>
2018-11-17 15:30:49 +0100

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +6,5 @@ import { TodoItem } from './interfaces/todo-item';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'Kate';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS', completed: false},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
 
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
 }
---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-list-manager',
+  templateUrl: './list-manager.component.html',
+  styleUrls: ['./list-manager.component.less']
+})
+export class ListManagerComponent implements OnInit {
+
+  constructor() { }
+
+  title = 'Kate';
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS', completed: false},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  addItem(title: string) {
+    this.todoList.push({ title });
+  }
+
+  ngOnInit() {
+  }
+
+}
